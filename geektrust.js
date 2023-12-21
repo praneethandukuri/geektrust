f844558b846b336bf6cf8ac7d31eed28b814c46b
praneethandukuri <praneethandukuri@gmail.com>
2023-12-21 13:52:42 +0530

    refactors hardcoded values of trains

--- geektrust.js
+++ geektrust.js
@@ -1,34 +1,29 @@
 const fs = require("fs")
 const { TRAIN_A, TRAIN_B } = require("./sample_input/trains.js")
 
-const getCurrentTrain = function (trainInput) {
+const getInitialTrains = function (trainInput) {
   const trains = trainInput.split("\n");
   const trainInputs = trains.map(currentTrain => (currentTrain.trimEnd().split(" ")))
 
   return trainInputs;
 }
 
-const getTrainBogies = function (currentTrain, train) {
-  const departedBogies = [];
-  const arrivalBogiesOrder = []
-
-  currentTrain.forEach(currentBoghie => {
-    if (train[currentBoghie] < train.HYB) {
-      departedBogies.push(currentBoghie);
-    } else {
-      arrivalBogiesOrder.push(currentBoghie);
-    }
+const getBogiesOrder = function (currentTrain) {
+  const arrivalBogiesOrder = currentTrain.filter(currentBoghie => {
+    return TRAIN_A[currentBoghie] > TRAIN_A.HYB || TRAIN_B[currentBoghie] > TRAIN_B.HYB
   });
 
-  return `ARRIVAL ${arrivalBogiesOrder.join(' ')}`;
+  return arrivalBogiesOrder.join(' ')
 }
 
 const main = function (filename) {
   const trainInput = fs.readFileSync(filename, "utf8")
-  const [trainA, trainB] = getCurrentTrain(trainInput);
+  const trains = getInitialTrains(trainInput);
 
-  console.log(getTrainBogies(trainA, TRAIN_A));
-  console.log(getTrainBogies(trainB, TRAIN_B));
+  trains.forEach(train => {
+    const bogiesOrder = getBogiesOrder(train)
+    console.log(`ARRIVAL ${train[0]} ENGINE ${bogiesOrder}`);
+  });
 }
 
 main(process.argv[2])
