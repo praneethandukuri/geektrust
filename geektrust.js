d7735cb4bc818554fdec3cbc4e75483545cbc131
praneethandukuri <praneethandukuri@gmail.com>
2023-12-21 13:24:42 +0530

    print trainA and trainB on arrival of hyderabad

--- geektrust.js
+++ geektrust.js
@@ -1,37 +1,34 @@
 const fs = require("fs")
 const { TRAIN_A, TRAIN_B } = require("./sample_input/trains.js")
 
-const filename = process.argv[2]
+const getCurrentTrain = function (trainInput) {
+  const trains = trainInput.split("\n");
+  const trainInputs = trains.map(currentTrain => (currentTrain.trimEnd().split(" ")))
 
-const trainInput = fs.readFileSync(filename, "utf8")
-
-const trainA = (trainInput.split("\n")[0].trimEnd().split(" "));
-const removedBhogisInTrainA = []
-const remainingBhogisInTrainA = []
-trainA.forEach(currentBoghie => {
-  if (TRAIN_A[currentBoghie] < TRAIN_A.HYB) {
-    removedBhogisInTrainA.push(currentBoghie);
-  } else {
-    remainingBhogisInTrainA.push(currentBoghie);
-  }
-});
-console.log(`ARRIVAL ${remainingBhogisInTrainA.join(' ')}`);
-// console.log(`ARRIVAL ${removedBhogisInTrainA.join(' ')}`);
-
-const trainB = (trainInput.split("\n")[1].trimEnd().split(" "));
-const removedBhogisInTrainB = []
-const remainingBhogisInTrainB = []
-trainB.forEach(currentBoghie => {
-  if (TRAIN_B[currentBoghie] < TRAIN_B.HYB) {
-    removedBhogisInTrainB.push(currentBoghie);
-  } else {
-    remainingBhogisInTrainB.push(currentBoghie);
-  }
-});
-console.log(`ARRIVAL ${remainingBhogisInTrainB.join(' ')}`);
-
-
-const functionName = function (params) {
-  return;
+  return trainInputs;
 }
-functionName()
+
+const getTrainBogies = function (currentTrain, train) {
+  const departedBogies = [];
+  const arrivalBogiesOrder = []
+
+  currentTrain.forEach(currentBoghie => {
+    if (train[currentBoghie] < train.HYB) {
+      departedBogies.push(currentBoghie);
+    } else {
+      arrivalBogiesOrder.push(currentBoghie);
+    }
+  });
+
+  return `ARRIVAL ${arrivalBogiesOrder.join(' ')}`;
+}
+
+const main = function (filename) {
+  const trainInput = fs.readFileSync(filename, "utf8")
+  const [trainA, trainB] = getCurrentTrain(trainInput);
+
+  console.log(getTrainBogies(trainA, TRAIN_A));
+  console.log(getTrainBogies(trainB, TRAIN_B));
+}
+
+main(process.argv[2])
